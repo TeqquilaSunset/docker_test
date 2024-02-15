57c2649d538a6bea779480973786a454dfb0461c
Vasiliy Bistritskiy <sc-2005@yandex.ru>
2024-02-15 14:20:40 +0700

    refactor(): del unused files

--- registerHealthCheck.ps1
+++
@@ -1,7 +0,0 @@
-# Регестрация проверки здоровья сервисов в консул
-Invoke-WebRequest -Method Put -InFile "consul_api_requests\health\rabbitHealth.json" -Uri "http://localhost:8500/v1/agent/check/register"
-
-Invoke-WebRequest -Method Put -InFile "consul_api_requests\health\serviceApiHealth.json" -Uri "http://localhost:8500/v1/agent/check/register"
-
-Invoke-WebRequest -Method Put -InFile "consul_api_requests\health\serviceFrontHealth.json" -Uri "http://localhost:8500/v1/agent/check/register"
-
