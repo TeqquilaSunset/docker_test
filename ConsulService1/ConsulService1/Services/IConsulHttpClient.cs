2ac44b9faf03abcd86fac49db2ab3a7559d8b156
Василий Быстрицкий <86839875+TeqquilaSunset@users.noreply.github.com>
2024-02-26 15:19:16 +0700

    Feat service1 (#12)
    * del(ConsulService1.http): unuse file
    
    * refactor(): add consul nuget

--- ConsulService1/ConsulService1/Services/IConsulHttpClient.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ConsulService1.Services
-{
-    public interface IConsulHttpClient
-    {
-        public Task RegisterServiceAsync();
-        public Task DeregisterServiceAsync();
-    }
-}
